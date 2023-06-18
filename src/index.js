7897384b7ae61772ac4a2f093ac2dfd4f60fd66b
brutalvik <vikcanada90@gmail.com>
2023-06-18 00:05:07 -0600

    [FEAT]: Added theme

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
+const theme = extendTheme({
+  colors: {
+    text: {
+      dark: "#000000",
+      light: "#ffffff",
+    },
+    background: {
+      dark: "#242425",
+      light: "#CBCBD0",
+    },
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ChakraProvider>
 );
