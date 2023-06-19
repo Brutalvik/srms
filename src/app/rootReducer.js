3c9ef95be26720dec2d06545e9beb2ae19109004
brutalvik <vikcanada90@gmail.com>
2023-06-19 00:20:02 -0600

    [FEAT]: added result thunk to post data

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "@reduxjs/toolkit";
 import theme from "./reducers/theme";
 import student from "./reducers/student";
 import course from "./reducers/course";
+import results from "./reducers/results";
 
 export const rootReducer = combineReducers({
   theme: theme,
   student: student,
   course: course,
+  results: results,
 });
---
+++ src/app/thunks/addResultThunk.js
@@ -0,0 +1,20 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { addResult } from "../reducers/results";
+
+export const addResultThunk = createAsyncThunk(
+  "addResult",
+  async ({ values, dispatch }) => {
+    try {
+      const { data, status } = await axios.post(
+        "http://localhost:5000/api/addresult",
+        values
+      );
+      dispatch(addCourse({ message: data.message, status: status }));
+      return data;
+    } catch (error) {
+      const { data, status } = await error.response;
+      dispatch(addCourse({ message: data.message, status: status }));
+    }
+  }
+);
